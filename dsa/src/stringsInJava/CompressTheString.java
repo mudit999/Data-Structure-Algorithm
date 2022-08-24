0da365af8838f1230882bfe2a1d435a3980287a9
mudit999 <muditgarg1211@gmail.com>
2022-08-24 13:54:01 +0530

    Strings

---
+++ dsa/src/stringsInJava/CompressTheString.java
@@ -0,0 +1,33 @@
+package stringsInJava;
+
+public class CompressTheString {
+	
+    public static String getCompressedString(String str) {
+    	String ans = "";
+        for(int i = 0 ; i<str.length();){
+            int count = 1;
+            int j = i+1;
+            for(;j<str.length();j++){
+                if(str.charAt(i) == str.charAt(j)){
+                    count++;
+                }else {
+                	break;
+                }
+            }
+            if(count == 1){
+                ans += str.charAt(i);
+            }else{
+                ans += str.charAt(i) + "" + count;
+            }
+            i = j;
+        }
+        return ans;
+    }
+
+	public static void main(String[] args) {
+		String str = "aaabcdd";
+		String ans = getCompressedString(str);
+		System.out.println(ans);
+	}
+
+}
---
+++ dsa/src/stringsInJava/printAllChars.java
@@ -0,0 +1,14 @@
+package stringsInJava;
+
+public class printAllChars {
+
+	public static void main(String[] args) {
+		String s1 = "mudit";
+		
+		for(int i = 0 ; i<s1.length();i++) {
+			System.out.print(s1.charAt(i) + " ");
+		}
+
+	} 
+
+}
