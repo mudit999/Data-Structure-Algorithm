1569ec800d26047f3acf6605a5d1f7c34a405b70
mudit999 <muditgarg1211@gmail.com>
2022-08-24 22:24:00 +0530

    Remove Leaf Node, Mirror/Inverted Binary Tree

---
+++ dsa/src/trees/BinaryTreeQues2.java
@@ -0,0 +1,43 @@
+package trees;
+
+public class BinaryTreeQues2 {
+		
+	public static void mirrorBinaryTree(BinaryTreeNode<Integer> root) {
+		if(root == null) {
+			return;
+		}
+	
+		// left call
+		mirrorBinaryTree(root.left);
+		// right call
+		mirrorBinaryTree(root.right);
+		
+		// processing the root/parent - swapping the nodes
+		BinaryTreeNode<Integer> temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+	}
+
+	public static BinaryTreeNode<Integer> removeLeafNode(BinaryTreeNode<Integer> root) {
+		if(root == null) {
+			return null;
+		}
+		
+		// root is a leaf
+		if(root.left == null && root.right == null) {
+			return null;
+		}
+		
+		root.left = removeLeafNode(root.left);
+		root.right = removeLeafNode(root.right);
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = BinaryTreeUse.takeTreeInputBetter(true,0,true);
+//		BinaryTreeNode<Integer> newRoot = removeLeafNode(root);
+		mirrorBinaryTree(root);
+		BinaryTreeUse.printTreeDetailed(root);
+	}
+
+}
