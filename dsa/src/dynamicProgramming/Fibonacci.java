396c95dfb0b15b8745284ccb11adf0d58dacb4c0
mudit999 <muditgarg1211@gmail.com>
2022-09-15 23:09:49 +0530

    Dynamic Programming - Fibonacci Ques

---
+++ dsa/src/dynamicProgramming/Fibonacci.java
@@ -0,0 +1,52 @@
+package dynamicProgramming;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static int fib(int n, int[] dp) {
+		// base case
+		if(n == 0 || n == 1) {
+			return n;
+		}
+		
+		int ans1, ans2;
+		
+		if(dp[n-1] == -1) {
+			// cal ans1
+			ans1 = fib(n-1, dp);
+			
+			// store it in array
+			dp[n-1] = ans1;
+		}else {
+			//get ans1
+			ans1 = dp[n-1];
+		}
+		
+		if(dp[n-2] == -1) {
+			ans2 = fib(n-2, dp);
+			dp[n-2] = ans2;
+		}else {
+			ans2 = dp[n-2];
+		}
+		
+		int finalAns = ans1 + ans2;
+		return finalAns;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		// array to store recursion call results
+		int[] dp = new int[n+1];
+		
+		// initialize whole array with -1
+		for(int i = 0; i<dp.length; i++) {
+			dp[i] = -1;
+		}
+		
+		System.out.println(fib(n, dp));
+	}
+
+}
